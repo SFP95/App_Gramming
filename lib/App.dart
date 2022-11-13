910de1dd5de0108f352d6b665badc47d3477570f
alumno <dam.sonia.fernandez@gmail.com>
2022-11-13 19:24:02 +0100

    commit init

--- lib/App.dart
+++ lib/App.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_views/Home.dart';
+import 'login_views/LogoWait.dart';
 
 class App extends StatelessWidget{
   App ({Key?key}):super (key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    return MaterialApp(
+      initialRoute: '/splashView',
+      routes: {
+        '/home': (context) => Home(),
+        //'/loginView': (context) => LoginView(),
+        '/logoWait': (context) => LogoWait("assets/images/.."),
+      },
+    );
   }
-  
 }
