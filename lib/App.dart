2f223d2d75f50d65dc8718b91289be2a99a2336e
alumno <dam.sonia.fernandez@gmail.com>
2022-11-13 19:06:04 +0100

    comit init

---
+++ lib/App.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class App extends StatelessWidget{
+  App ({Key?key}):super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'App.dart';
+
+void main () async{
+  WidgetsFlutterBinding.ensureInitialized();
+/*
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );*/
+
+  runApp(
+      App());
+}
+
