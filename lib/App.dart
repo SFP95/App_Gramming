829fc9dd5a0c58b1762dc2aed89245fdf04dfd68
alumno <dam.sonia.fernandez@gmail.com>
2022-11-17 18:47:04 +0100

    upload load imgs and doc json for quizs

--- lib/App.dart
+++ lib/App.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'Views/LogoWait.dart';
 import 'home_views/Home.dart';
-import 'login_views/LogoWait.dart';
+
 
 class App extends StatelessWidget{
   App ({Key?key}):super (key: key);
@@ -15,7 +15,7 @@ class App extends StatelessWidget{
       routes: {
         '/home': (context) => Home(),
         //'/loginView': (context) => LoginView(),
-        '/logoWait': (context) => LogoWait("assets/images/.."),
+        '/logoWait': (context) => LogoWait("assets/images/LogoGraming3.png"),
       },
     );
   }
