c8085ecd3bab14f9bf0eed7420c354a08b950626
alumno <dam.sonia.fernandez@gmail.com>
2022-11-18 23:01:09 +0100

    18.11.22

--- lib/pages/QuizPage.dart
+++ lib/pages/QuizPage.dart
@@ -19,6 +19,7 @@ class _QuizPageState extends State<QuizPage> {
         title: const Text('QUIZs'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 30),
@@ -31,7 +32,50 @@ class _QuizPageState extends State<QuizPage> {
                 )
             ),
           ),
-          Container(),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 450),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25,vertical: 20),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(15),
+                      child: const Text('PREGUNTA 1'),
+                    ),
+                    Flexible(
+                      child: ListView.builder(
+                        itemCount: 4,
+                        itemBuilder: (_, index) {
+                    return Container(
+                      margin:  const EdgeInsets.all(9),
+                      decoration:  BoxDecoration(
+                        border: Border.all(
+                          color: Colors.deepPurpleAccent,width: 2),
+                        borderRadius: BorderRadius.circular(15),
+                        
+                      ),
+                      child: ListTile(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15),
+                          ),
+                        ),
+                        leading: Text('${index + 1}'),
+                        title: Text('Respuesta'),
+                        onTap: (){
+
+                        },
+                      ),
+                    );
+                    },
+                    ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           TextButton(
             onPressed: () {  },
             child: const Text('Skip'),
