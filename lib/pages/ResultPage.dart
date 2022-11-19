01e7fde295b1532dab195da83ef37157752073b9
alumno <dam.sonia.fernandez@gmail.com>
2022-11-19 13:07:20 +0100

    19.11.22

--- lib/pages/ResultPage.dart
+++ lib/pages/ResultPage.dart
@@ -11,7 +11,58 @@ class ResultPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      backgroundColor: Colors.deepPurpleAccent[100],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent[100],
+        elevation: 0,
+        title:  Text(quiz.name),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding:  const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(left: 2,right: 2,top: 2,bottom: 10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.deepPurple.shade100,
+                  width: 1,
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('Quizs: ${quiz.questions.length}',
+                    style: Theme.of(context).textTheme.headline2,),
+                  Text('Corrects: ${quiz.percent} %',
+                    style: Theme.of(context).textTheme.headline2,),
+                ],
+              ),
+            ),
+            Expanded(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: quiz.questions.length,
+                    itemBuilder: (_,index){
+                      return Card(
+                        color: quiz.questions[index].correct ? Colors.green.shade300 : Colors.red.shade300,
+                        child: ListTile(
+                          leading: quiz.questions[index].correct ?
+                          Icon(Icons.check,
+                            color: Colors.green.shade700) :
+                          Icon(Icons.close,
+                              color: Colors.red.shade700),
+                        title: Text(quiz.questions[index].question),
+                          subtitle: Text(quiz.questions[index].select),
+                          trailing: Text(quiz.questions[index].answer),
+                        ),
+                        );
+                    }
+                ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
